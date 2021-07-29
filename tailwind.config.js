36e9d0e93ac7ccaf2ce45680a45113d8dcc9e4ea
Nursultan <nmukhamedali@gmail.com>
2021-07-29 13:43:02 +0600

    soe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   purge: [
-    "./src/**/*.css", 
+    "./src/**/*.css",
     "./**/*.html",
     './src/**/*.js',
   ],
@@ -9,7 +9,7 @@ module.exports = {
     container: {
       center: true,
     },
-    colors:{
+    colors: {
       black: "#000",
       white: "#fff",
       blue: "#0371FF",
@@ -24,54 +24,54 @@ module.exports = {
       main_color: "#00BCD4"
     },
     fontSize: {
-      'sm' : '14px',
-      'md' : '16px',
+      'sm': '14px',
+      'md': '16px',
       'mdx': '20px',
-      'lg' : '2rem',
+      'lg': '2rem',
       'rem2Half': '2.5rem',
-      'rem1Half' : '1.5rem',
-      'rem1' : '1rem',
-      '0.7rem' : '0.7rem',
-      '0.5rem' : '0.5rem',
-      '0.2rem' : '0.2rem',
-      '0.9rem' : '0.9rem',
-      '1.2rem' : '1.2rem',
-      '1.4rem' : '1.4rem',
-      '1.6rem' : '1.6rem',
-      '2.2rem' : '2.2rem',
-      '2.4rem' : '2.4rem',
-      '2rem' : '2rem',
-      '2.6rem' : '2.6rem',
-      '2.8rem' : '2.8rem'
+      'rem1Half': '1.5rem',
+      'rem1': '1rem',
+      '0.7rem': '0.7rem',
+      '0.5rem': '0.5rem',
+      '0.2rem': '0.2rem',
+      '0.9rem': '0.9rem',
+      '1.2rem': '1.2rem',
+      '1.4rem': '1.4rem',
+      '1.6rem': '1.6rem',
+      '2.2rem': '2.2rem',
+      '2.4rem': '2.4rem',
+      '2rem': '2rem',
+      '2.6rem': '2.6rem',
+      '2.8rem': '2.8rem'
     },
-    lineHeight:{
-      '1rem' : '1rem',
-      '1.2rem' : '1.2rem'
+    lineHeight: {
+      '1rem': '1rem',
+      '1.2rem': '1.2rem'
     },
-    borderRadius:{
-      '1rem' : '1rem',
-      '0.5rem' : '0.5rem',
-      '0.7rem' : '0.7rem',
-      '2rem' : '2rem',
-      '1.2rem' : '1.2rem',
-      '2.2rem' : '2.2rem',
-      '2.4rem' : '2.4rem',
-      '0.2rem' : '0.2rem',
-      '2.6rem' : '2.6rem',
-      '2.8rem' : '2.8rem',
+    borderRadius: {
+      '1rem': '1rem',
+      '0.5rem': '0.5rem',
+      '0.7rem': '0.7rem',
+      '2rem': '2rem',
+      '1.2rem': '1.2rem',
+      '2.2rem': '2.2rem',
+      '2.4rem': '2.4rem',
+      '0.2rem': '0.2rem',
+      '2.6rem': '2.6rem',
+      '2.8rem': '2.8rem',
       '296px': '296px',
-      'full' : '9999px'
+      'full': '9999px'
     },
     spacing: {
-      xs : '4px',
+      xs: '4px',
       rem1: "1rem",
-      rem2 : "2rem",
-      remHalf : '0.5rem',
+      rem2: "2rem",
+      remHalf: '0.5rem',
       '296px': '296px',
-      '0.2rem' : '0.2rem',
+      '0.2rem': '0.2rem',
       'rem2Half': '2.5rem',
-      '19rem' : '19rem',
-      'heightImage' : '17.4375rem',
+      '19rem': '19rem',
+      'heightImage': '17.4375rem',
       xsRem: '3rem',
       none: '0px',
       sm: '8px',
@@ -80,46 +80,49 @@ module.exports = {
       xl: '48px',
       footer_img_w: '190px',
       half: "50%",
-      '0.7rem' : '0.7rem',
-      '0.5rem' : '0.5rem',
-      '0.9rem' : '0.9rem',
-      '1.2rem' : '1.2rem',
-      '2.2rem' : '2.2rem',
-      '1.2rem' : '1.2rem',
-      '1.4rem' : '1.4rem',
-      '1rem' : '1rem',
-      '2rem' : '2rem',
-      '2.4rem' : '2.4rem',
-      '2.6rem' : '2.6rem',
-      '2.8rem' : '2.8rem',
-      '3rem' : '3rem',
-      '3.2rem' : '3.2rem',
-      '3.4rem' : '3.4rem',
+      '0.7rem': '0.7rem',
+      '0.5rem': '0.5rem',
+      '0.9rem': '0.9rem',
+      '1.2rem': '1.2rem',
+      '2.2rem': '2.2rem',
+      '1.2rem': '1.2rem',
+      '1.4rem': '1.4rem',
+      '1rem': '1rem',
+      '2rem': '2rem',
+      '2.4rem': '2.4rem',
+      '2.6rem': '2.6rem',
+      '2.8rem': '2.8rem',
+      '3rem': '3rem',
+      '3.2rem': '3.2rem',
+      '3.4rem': '3.4rem',
       'searchItem': '304px',
-      'heightItem' : '36px'
+      'heightItem': '36px'
     },
-    screens:{
-      'phone' : {'max' : '639px'},
-      'mobile' : {
+    screens: {
+      'phone': { 'max': '639px' },
+      'mobile': {
         'min': '481px',
-        'max' : '767px'
+        'max': '767px'
       },
-      'tablet' : {
+      'tablet': {
         'min': '768px',
         'max': '1024px'
       },
-      'fromTabletToPhone':{
+      'fromTabletToPhone': {
         'max': '1024px'
       },
-      'laptop' : {'max': '1023px'},
-      'desktop': {'max': '1279px'},
+      'laptop': { 'max': '1023px' },
+      'desktop': { 'max': '1279px' },
       'icon_stack': {
-        'min' : '640px',
-        'max' : '767px'
+        'min': '640px',
+        'max': '767px'
       },
-      'desktop_custom':{
+      'desktop_custom': {
         'min': '1024px',
-        'max' : '1279px'
+        'max': '1279px'
+      },
+      'tablet_min': {
+        'min': '992px'
       }
     }
   },
